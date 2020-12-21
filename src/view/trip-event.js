91cd5a3a1b1ff20d6ff7ced49ff63eca5d51f342
gip2 <gabitoff@yandex.ru>
2020-12-21 17:04:45 +0500

    c

--- src/view/trip-event.js
+++ src/view/trip-event.js
@@ -1,10 +1,16 @@
 const getMonthAndDay = (date) => date.toDateString().slice(4, 10).toUpperCase();
 const getTime = (date) => date.toTimeString().slice(0, 5);
+
+const msecInSecond = 1000;
+const msecInMinute = 60 * msecInSecond;
+const minutesInHour = 60;
+const minutesInDay = 24 * minutesInHour;
+
 const durationTemplate = (date1, date2) => {
-  let durationMinutes = Math.floor((date2 - date1) / 60000);
-  let days = Math.floor(durationMinutes / 1440);
-  let hours = Math.floor((durationMinutes - days * 1440) / 60);
-  let minutes = durationMinutes - days * 1440 - hours * 60;
+  let durationMinutes = Math.floor((date2 - date1) / msecInMinute);
+  let days = Math.floor(durationMinutes / minutesInDay);
+  let hours = Math.floor((durationMinutes - days * minutesInDay) / minutesInHour);
+  let minutes = durationMinutes - days * minutesInDay - hours * minutesInHour;
   if (days > 0) {
     return String(days) + `D ` + hours + `H ` + minutes + `M`;
   }
@@ -13,21 +19,19 @@ const durationTemplate = (date1, date2) => {
   }
   return String(minutes) + `M`;
 };
+
 const createOffersListTemplate = (offers) => {
   if (offers !== undefined) {
     let liString = ``;
-    offers.forEach(element => {
+    offers.forEach((element) => {
+      const {name, price} = element;
       liString += `<li class="event__offer">
-        <span class="event__offer-title">` +
-        element.name + `</span>
+        <span class="event__offer-title">${name}</span>
         +€&nbsp;
-        <span class="event__offer-price">` +
-        element.price + `</span>
+        <span class="event__offer-price">${price}</span>
       </li>`;
     });
-    return `<ul class="event__selected-offers">` +
-      liString +
-    `</ul>`;
+    return `<ul class="event__selected-offers">${liString}</ul>`;
   }
   return ``;
 };
@@ -38,49 +42,28 @@ const favoritySolidTemplate = (solidFlag) => {
   return ``;
 };
 
-export const createTripEventTemplate = (event) =>
-  `<div class="event">
-    <time class="event__date" datetime=` +
-    event.dateBegin +
-    `>` + getMonthAndDay(event.dateBegin) +
-    `</time>
+export const createTripEventTemplate = (event) => {
+  const {dateBegin, typeIconSrc, typeText, location, dateEnd, price,offers, favority} = event;
+  return `<div class="event">
+    <time class="event__date" datetime=${dateBegin}>${getMonthAndDay(dateBegin)}</time>
     <div class="event__type">
-      <img class="event__type-icon" width="42" height="42" src="` +
-      event.typeIconSrc +
-      `" alt="Event type icon">
+      <img class="event__type-icon" width="42" height="42" src="${typeIconSrc}" alt="Event type icon">
     </div>
-    <h3 class="event__title">` +
-    event.typeText + ` ` +
-    event.location +
-    `</h3>
+    <h3 class="event__title">${typeText} ${location}</h3>
     <div class="event__schedule">
       <p class="event__time">
-        <time class="event__start-time" datetime="` +
-        event.dateBegin + // 2019-03-18T10:30
-        `">` +
-        getTime(event.dateBegin) +
-        `</time>
+        <time class="event__start-time" datetime="${dateBegin}">${getTime(dateBegin)}</time>
         —
-        <time class="event__end-time" datetime="` +
-        event.dateEnd + // 2019-03-18T10:30
-        `">` +
-        getTime(event.dateEnd) +
-        `</time>
+        <time class="event__end-time" datetime="${dateEnd}">${getTime(dateEnd)}</time>
       </p>
-      <p class="event__duration">` +
-      durationTemplate(event.dateBegin, event.dateEnd) +
-      `</p>
+      <p class="event__duration">${durationTemplate(dateBegin, dateEnd)}</p>
     </div>
     <p class="event__price">
-      €&nbsp;<span class="event__price-value">` +
-      event.price +
-      `</span>
+      €&nbsp;<span class="event__price-value">${price}</span>
     </p>
-    <h4 class="visually-hidden">Offers:</h4>` +
-    createOffersListTemplate(event.offers) +
-    `<button class="event__favorite-btn event__favorite-btn` +
-    favoritySolidTemplate(event.favority) +
-    `" type="button">
+    <h4 class="visually-hidden">Offers:</h4>
+    ${createOffersListTemplate(offers)}
+    <button class="event__favorite-btn event__favorite-btn${favoritySolidTemplate(favority)}" type="button">
       <span class="visually-hidden">Add to favorite</span>
       <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
         <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
@@ -89,4 +72,4 @@ export const createTripEventTemplate = (event) =>
     <button class="event__rollup-btn" type="button">
       <span class="visually-hidden">Open event</span>
     </button>
-  </div>`;
+  </div>`};
